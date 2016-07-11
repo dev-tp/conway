55ce4b4d1a88c78530d11212293ed874901f353c
Eryk <rcksorto@yahoo.com>
2016-07-11 01:12:38 -0700

    TODO: better sleep() and find a way to clear terminal

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
+#include <time.h>
 
 #include "conway.h"
 
-int main() {
-    Life *life = new_life(40, 15);
-    if (life)
-        print_grid(life);
+void msleep(unsigned long seconds) {
+    unsigned long retention_time = time(0) + seconds;
+    while (time(0) < retention_time);
+}
 
-    for (int i = 0; i < 50; i++) {
-        life->step(life);
-        print_grid(life);
-        printf("\n");
+int main(int argc, char *argv[]) {
+    Life *life = new_life(15, 40);
+    for (int i = 0; i < 10; i++) {
+        step(life);
+        print_grid(life->first_field->grid, life->width, life->height);
+        // printf("- - - - - - - - - - - - - - - - - - - -\n");
+        // msleep(1);
+        // system("clear");
     }
-
     release_life(life);
     return 0;
 }
